d95b21aaf98cde3d2139d279af7428c4ceb56bd5
Davi Coelho <davipernalonga30@outlook.com>
2022-10-03 21:12:00 -0300

    Menu básico para o console

--- src/pkg2daudioplayer/Main.java
+++ src/pkg2daudioplayer/Main.java
@@ -5,6 +5,7 @@
 package pkg2daudioplayer;
 
 import java.io.IOException;
+import java.util.Scanner;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import pkg2daudioplayer.src.AudioPlayback;
@@ -20,7 +21,30 @@ public class Main {
      */
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         String filepath = "BlocoClimatizarBoscoDefar.wav";
-        AudioPlayback aud = new AudioPlayback(filepath);
+        AudioPlayback aud = new AudioPlayback();
+        aud.ChangeSong(filepath);
+        int i;
+        
+        Scanner input = new Scanner(System.in);
+        while(true){
+            System.out.println("1. Iniciar Playback");
+            System.out.println("2. Pausar Playback");
+            System.out.println("3. Resumir Playback");
+            System.out.println("4. Parar Playback");
+            System.out.println("5. Mudar Música");
+            System.out.println("6. Configurações");
+            System.out.println("0. Sair");
+            i = input.nextInt();
+            if(i == 0){
+                aud.StopPlayback();
+                break;
+            }
+            aud.Choice(i);
+            
+            
+            
+            
+        }
         
         // TODO code application logic here
     }
--- src/pkg2daudioplayer/src/AudioPlayback.java
+++ src/pkg2daudioplayer/src/AudioPlayback.java
@@ -6,6 +6,7 @@ package pkg2daudioplayer.src;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -20,38 +21,73 @@ public class AudioPlayback {
     private String filepath;
     private AudioInputStream audInput;
     private Clip clip;
+        
+    public void ChangeSong(String filepath) throws LineUnavailableException, IOException, UnsupportedAudioFileException{
+        this.filepath = filepath;
+        StartPlayback();
+        
+    }
     
-    public AudioPlayback() throws UnsupportedAudioFileException, IOException, LineUnavailableException{
+    public void StartPlayback() throws UnsupportedAudioFileException, IOException, LineUnavailableException{
+        System.out.println("Tocando agora: "+new File(filepath).getName());
         this.audInput = AudioSystem.getAudioInputStream(new File(this.filepath).getAbsoluteFile());
         this.clip = AudioSystem.getClip();
-        this.clip.open(audInput);
+        this.clip.open(this.audInput);
         this.clip.loop(Clip.LOOP_CONTINUOUSLY);
-        
-    }
-    
-    public void ChangeSong(String filepath){
-        
-    }
-    
-    public void StartPlayback(){
-        System.out.println("Tocando agora: "+new File(filepath).getName());
         this.clip.start();
     }
     
     public void PausePlayback(){
-        
+        long s = this.clip.getMicrosecondPosition() / 100000;
+        System.out.println("Playback Pausado. Tempo: "+s);
+        this.clip.stop();
     }
     
     public void ResumePlayback(){
+        long s = this.clip.getMicrosecondPosition() / 100000;
+        System.out.println("Playback Resumido. Tempo: "+s);
+        this.clip.start();
         
     }
     
     public void StopPlayback(){
+        System.out.println("Playback Parado. Música fora da lista de reprodução.");
+        this.filepath = "";
+        this.clip.stop();
+        this.clip.close();
         
     }
     
-    public void JumpToTime(){
+    public void JumpToTime(long timeMiliSeconds){
         
+    }
+    
+    public void Choice(int i) throws LineUnavailableException, IOException, UnsupportedAudioFileException{
+        switch(i){
+            case 1:
+                StartPlayback();
+                return;
+            case 2:
+                PausePlayback();
+                return;
+            case 3:
+                ResumePlayback();
+                return;
+            case 4:
+                StopPlayback();
+                return;
+            case 5:
+                System.out.println("Coloque o endereço da música. (arquivo .wav)");
+                Scanner input = new Scanner(System.in);
+                String aux = input.nextLine();
+                StopPlayback();
+                ChangeSong(aux);
+                return;
+            case 6:
+                System.out.println("Aqui vai a parte de configurações.");
+                return;
+           
+        }
     }
     
     
